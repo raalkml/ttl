89326976a4cdfcd87cc5039f8836a34370549f58
Alex Riesen <raa.lkml@gmail.com>
2014-03-07 17:21:44 +0100

    Explicitly instantiate some templates in test_ttl to monitor object code size

--- t/test_ttl.cpp
+++ t/test_ttl.cpp
@@ -3,6 +3,17 @@
 #include "ttl/ttl.hpp"
 #include "t.hpp"
 
+template class ttl::array<int, 3>;
+template class ttl::forward_list<testtype>;
+template class ttl::backward_list<testtype>;
+template class ttl::bitset<256>;
+template class ttl::lazy_queue<testtype>;
+template class ttl::pair<int,char>;
+template class ttl::pair_vector<char, int>;
+template class ttl::vector<testtype>;
+template class ttl::fixed_vector<testtype, 16>;
+// template class ttl::map<int, char>; // incomplete rbtree
+
 void test()
 {
    printf("ttl::size_t %lu\n", (unsigned long)sizeof(ttl::size_t));
