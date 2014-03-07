a16420db6e789ea95b1da2ed47cf064459514ac1
Alex Riesen <raa.lkml@gmail.com>
2014-03-07 13:28:19 +0100

    Fix, extend and cleanup test_array

--- t/test_array.cpp
+++ t/test_array.cpp
@@ -4,34 +4,53 @@
 
 void test()
 {
+   puts("initialization");
    ttl::array<int, 3> i3 = { {1,2,3} };
 
    for (ttl::array<int, 3>::const_iterator i = i3.begin(); i != i3.end(); ++i)
       printf("%ld/%lu: %d\n", long(i - i3.begin()), (unsigned long)i3.size(), *i);
 
-   ttl::array<int, 3>::iterator j = i3.begin();
-   for (ttl::array<int, 3>::const_iterator i = i3.cbegin(); i != i3.cend(); ++i)
-      assert(*i == *j++);
-   assert(j == i3.end());
+   puts("iterators");
+   {
+      ttl::array<int, 3>::iterator j = i3.begin();
+      for (ttl::array<int, 3>::const_iterator i = i3.cbegin(); i != i3.cend(); ++i)
+         assert(*i == *j++);
+      assert(j == i3.end());
+   }
 
-   ttl::array<int, 3> i30;
-   i30 = i3;
-   assert(i3 == i30);
+   puts("assignment operator");
+   {
+      ttl::array<int, 3> i30;
+      i30 = i3;
+      assert(i3 == i30);
 
-   ttl::array<int, 3> i31;
-   i31.swap(i3);
-   assert(i31 == i30);
+      puts("swap");
+      {
+         puts("initialization");
+         ttl::array<int, 3> i31;
+         i31.swap(i3);
+         assert(i31 == i30);
+      }
+   }
 
-   ttl::array<testtype, 3> ta;
-   ta.fill(testtype(9));
-   assert(ta[0].value == 9);
-   assert(ta[2] == ta[0]);
-   assert(!ta.empty());
-   assert(ta.size() == 3);
-   assert(ta.max_size() == ta.size());
+   puts("fill");
+   {
+      ttl::array<testtype, 3> ta;
+      ta.fill(testtype(9));
+      assert(ta[0].value == 9);
+      assert(ta[2] == ta[0]);
+      assert(!ta.empty());
+      assert(ta.size() == 3);
+      assert(ta.max_size() == ta.size());
 
-   ta = ttl::array<testtype, 3>();
-   assert(ta[0] == int());
-   assert(ta[1] == int());
-   assert(ta[2] == int());
+      puts("assignment and default element ctors");
+      {
+         ta = ttl::array<testtype, 3>();
+         assert(ta[0] == testtype());
+         assert(ta[1] == testtype());
+         assert(ta[2] == testtype());
+      }
+   }
+
+   puts("dtors");
 }
