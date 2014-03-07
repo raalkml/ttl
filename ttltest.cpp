af2b531c3602fc71a89327df65985d25fa3e7c05
Alex Riesen <raa.lkml@gmail.com>
2014-03-07 13:18:15 +0100

    Remove red-black tree from ttltest
    test_rbtree is much more interesting

--- ttltest.cpp
+++ ttltest.cpp
@@ -17,7 +17,6 @@
 
 #include <bitset>
 #include "ttl/ttl.hpp"
-#include "ttl/rbtree.hpp"
 
 struct testtype
 {
@@ -710,82 +709,6 @@ static void test_types()
    printf("uint: is_unsigned: %d\n", ttl::is_unsigned<unsigned int>::value);
 }
 
-typedef ttl::rbtree<int,
-        ttl::pair<int,char>,
-        ttl::select_first< ttl::pair<int,char> >,
-        ttl::less<int> > rbtree_map;
-
-template<typename T>
-struct select_same
-{
-   const T &operator()(const T &r) const { return r; }
-};
-
-typedef ttl::rbtree<int, int, select_same<int>, ttl::less<int> > rbtree_set;
-
-template<typename Container>
-static void inorder(const ttl::rbtree_base::const_link &h, int depth = 0)
-{
-   ttl::rbtree_base::const_link c = h.left();
-   if (*c)
-      inorder<Container>(c, depth + 1);
-   for (int i = depth; i--;)
-      fputc(' ', stdout);
-   typename Container::keyof_type keyof;
-   printf("%d\n", keyof(static_cast<const typename Container::node *>(*h)->data));
-   c = h.right();
-   if (*c)
-      inorder<Container>(c, depth + 1);
-}
-
-static void test_rbtree()
-{
-   printf("sizeof rbnode %lu, rbtree_map::node %lu, rbtree_set::node %lu\n",
-          (unsigned long)sizeof(ttl::rbnode),
-          (unsigned long)sizeof(rbtree_map::node),
-          (unsigned long)sizeof(rbtree_set::node));
-   rbtree_map t;
-   for (unsigned c = 10; c--; )
-   {
-      ttl::pair<int,char> d(c, -c);
-      rbtree_map::node *pos = t.insert_equal(d);
-      printf("%p %d (%d)\n", pos, d.first, d.second);
-   }
-
-   printf("red-black tree with 10 elements\n");
-   inorder<rbtree_map>(t.get_croot());
-
-   ttl::pair<int,char> dupe(5, -5);
-   assert(NULL == t.insert_unique(dupe));
-   assert(NULL != t.insert_equal(dupe));
-
-   printf("inserted dupe (5)\n");
-   inorder<rbtree_map>(t.get_croot());
-
-   t.delete_min(t.get_root().pos);
-   t.delete_min(t.get_root().pos);
-   t.delete_min(t.get_root().pos);
-
-   printf("3 calls to delete_min from root\n");
-   inorder<rbtree_map>(t.get_croot());
-
-   t.remove(7);
-   printf("remove 7\n");
-   inorder<rbtree_map>(t.get_croot());
-
-   rbtree_set s;
-   s.insert_unique(1);
-   s.insert_unique(2);
-   s.insert_unique(3);
-   s.insert_unique(3);
-   printf("set of three elements\n");
-   inorder<rbtree_set>(s.get_croot());
-
-   s.remove(1);
-   printf("set of two elements\n");
-   inorder<rbtree_set>(s.get_croot());
-}
-
 static const struct
 {
    const char *name;
@@ -803,7 +726,6 @@ static const struct
    { "bitset", &test_bitset },
    { "countof", &test_countof },
    { "types",  &test_types },
-   { "rbtree",  &test_rbtree },
 };
 
 int main(int argc, char* argv[], char* envp[])
