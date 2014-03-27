c877b1b31ffaed820f8994ec5139d641b885ea8b
Alex Riesen <raa.lkml@gmail.com>
2014-03-27 14:50:06 +0100

    More algorithm tests

--- t/test_algo.cpp
+++ t/test_algo.cpp
@@ -1,12 +1,20 @@
 #include "t.hpp"
-#include "ttl/algorithm.hpp"
 #include "ttl/utility.hpp"
+#include "ttl/functional.hpp"
+#include "ttl/algorithm.hpp"
 
 struct less_than
 {
    int v;
    less_than(int v_): v(v_) {}
    bool operator()(int o) const { return o < v; }
+};
+
+struct equal_to
+{
+   int v;
+   equal_to(int v_): v(v_) {}
+   bool operator()(int o) const { return o == v; }
 };
 
 static void print_ints(const char *title, const int *first, const int *last)
@@ -17,8 +25,15 @@ static void print_ints(const char *title, const int *first, const int *last)
    fputs(".\n", stdout);
 }
 
+struct printer
+{
+   void operator()(const int &v) { printf(" %d", v); }
+};
+
 void test()
 {
+   int *p;
+
    int a0[] = {1,2,3,4};
    int a1[countof(a0)] = {0};
    ttl::copy(a0, a0 + countof(a0), a1);
@@ -40,35 +55,82 @@ void test()
    assert(a0[2] == a1[2] && a1[0] == 0);
    print_ints("copy_backward:", a1, a1 + countof(a1));
 
+   p = ttl::remove_copy(a0, a0 + countof(a0), a1, 3);
+   print_ints("remove_copy(3):", a1, p);
+   assert(ttl::find(a1, p, 3) == p);
+
+   p = ttl::remove_copy_if(a0, a0 + countof(a0), a1, less_than(3));
+   print_ints("remove_copy_if(< 3):", a1, p);
+   assert(ttl::find(a1, p, 1) == p);
+   assert(ttl::find(a1, p, 2) == p);
+   assert(ttl::find(a1, p, 3) == a1);
+
    int a2[] = {1,2,2,3,4,6};
-   print_ints("a2:", a2, a2 + countof(a2));
-   int *p = ttl::lower_bound(a2, a2 + countof(a2), 2);
+   int *a2end = a2 + countof(a2);
+   print_ints("a2:", a2, a2end);
+
+   assert(ttl::count(a2, a2end, 2) == 2);
+   assert(ttl::count_if(a2, a2end, less_than(2)) == 1);
+   assert(ttl::any_of(a2, a2end, equal_to(4)));
+   assert(ttl::none_of(a2, a2end, equal_to(5)));
+   assert(*ttl::adjacent_find(a2, a2end) == 2);
+   assert(*ttl::adjacent_find(a2, a2end, ttl::equal_to<int>()) == 2);
+
+   fputs("for_each", stdout);
+#if __cplusplus >= 201103L // C++11
+   fputs("(lambda):", stdout);
+   ttl::for_each(a2, a2end, [](const int &v){ printf(" %d", v); } );
+#else
+   fputs("(class inst):", stdout);
+   ttl::for_each(a2, a2end, printer());
+#endif
+   fputs("\n", stdout);
+
+   p = ttl::lower_bound(a2, a2end, 2);
    printf("a2 dup lower_bound: [%ld\n", (long)(p - a2));
    assert(p - a2 == 1 && *p == 2);
-   p = ttl::lower_bound(a2, a2 + countof(a2), 5); // missing, in-range
+   p = ttl::lower_bound(a2, a2end, 5); // missing, in-range
    assert(p == a2 + 5);
-   p = ttl::lower_bound(a2, a2 + countof(a2), 7); // missing, out-of-range
-   assert(p == a2 + countof(a2));
+   p = ttl::lower_bound(a2, a2end, 7); // missing, out-of-range
+   assert(p == a2end);
 
-   p = ttl::upper_bound(a2, a2 + countof(a2), 2);
+   p = ttl::upper_bound(a2, a2end, 2);
    printf("a2 dup upper_bound: %ld)\n", (long)(p - a2));
    assert(p - a2 == 3);
-   p = ttl::upper_bound(a2, a2 + countof(a2), 5); // missing, in-range
+   p = ttl::upper_bound(a2, a2end, 5); // missing, in-range
    assert(p == a2 + 5);
-   p = ttl::upper_bound(a2, a2 + countof(a2), 7); // missing, out-of-range;
-   assert(p == a2 + countof(a2));
+   p = ttl::upper_bound(a2, a2end, 7); // missing, out-of-range;
+   assert(p == a2end);
 
-   ttl::pair<int *, int *> r = ttl::equal_range(a2, a2 + countof(a2), 2);
+   ttl::pair<int *, int *> r = ttl::equal_range(a2, a2end, 2);
    printf("a2 dup equal_range: [%ld,%ld)\n", (long)(r.first - a2), (long)(r.second - a2));
+   assert(ttl::all_of(r.first, r.second, equal_to(2)));
    assert(r.second - r.first == 2);
    assert(*r.first == 2);
    assert(*(r.first + 1) == 2);
 
-   r = ttl::equal_range(a2, a2 + countof(a2), 5);
+   r = ttl::equal_range(a2, a2end, 5);
    assert(r.first == r.second);
    assert(r.first == a2 + 5);
 
-   r = ttl::equal_range(a2, a2 + countof(a2), 7);
+   r = ttl::equal_range(a2, a2end, 7);
    assert(r.first == r.second);
-   assert(r.first == a2 + countof(a2));
+   assert(r.first == a2end);
+
+   p = ttl::remove(a2, a2end, 2);
+   print_ints("a2 remove 2:", a2, p);
+   assert(ttl::find(a2, p, 2) == p);
+   assert(ttl::find(a2, p, 6) + 1 == p);
+   assert(a2[1] == 3);
+   assert(*ttl::find(a2, p, 6) == 6);
+   a2end = p;
+
+   p = ttl::remove_if(a2, a2end, less_than(2));
+   print_ints("a2 remove less than 2:", a2, p);
+   assert(ttl::find(a2, p, 2) == p);
+   assert(ttl::find(a2, p, 6) + 1 == p);
+   assert(a2[0] == 3);
+   assert(*ttl::find(a2, p, 6) == 6);
+   a2end = p;
+
 }
