29703fc2267d96954c73c6565036b6498130e16e
Alex Riesen <raa.lkml@gmail.com>
2014-04-07 16:12:02 +0200

    Clarify origins of the code and choice of the license (public domain)
    and add some comments

--- ttl/ttl.hpp
+++ ttl/ttl.hpp
@@ -7,15 +7,26 @@
 // A library of STL-inspired templates designed to work in conditions
 // of constrained resources, but also without RTTI and exceptions.
 //
-// Some templates are implementations of STL, some are not.
+// All of the used algorithms and data structures either have prior art
+// or are trivial and well known.
+//
+// Care was taken to avoid patented or prohibitively licensed code,
+// even if this meant suboptimal code.
+//
+//
+// Some templates are implementations of STL interfaces, some are not.
 //
 // There is no allocator support and the O(log N) and similar complexity
-// guarantees might be not implemented. Also reverse iterators might be
-// missing.
+// guarantees might be not implemented.
+//
+// The reverse iterators are not implemented.
 //
 // Usually only equality operators are implemented, if any.
 //
 // There is generally no streaming operator support.
+//
+// This header is optional. The feature headers below should be usable on
+// their own, unless the code needs features from the others.
 //
 #ifndef _TINY_TEMPLATE_LIBRARY_HPP_
 #define _TINY_TEMPLATE_LIBRARY_HPP_ 1
