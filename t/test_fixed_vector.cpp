7e05869bc8f66cf88b0eae54e83bd186bfcb4e52
Alex Riesen <raa.lkml@gmail.com>
2014-04-08 12:14:55 +0200

    Improve tests of fixed_vector

--- t/test_fixed_vector.cpp
+++ t/test_fixed_vector.cpp
@@ -10,31 +10,49 @@ void test()
 
    printf("default ctor:\n");
    testvector v0;
+   testtype::verbose = false;
+
    v0.resize(3, testtype(2));
+   assert(v0.size() == 3);
    v0.resize(1);
+   assert(v0.size() == 1);
    v0.insert(v0.begin(), testtype(0));
+   assert(v0.size() == 2 && v0[0] == 0 && v0[1] == 2);
    for (testvector::const_iterator i = v0.cbegin(); i != v0.cend(); ++i)
       printf(" %d", i->value);
+   printf("\n");
    testvector vCopy = v0;
-   printf("size of a copy: %lu\n", (unsigned long)vCopy.size());
-   printf("copy[0]: %d\n", vCopy[0].value);
-   printf("copy[1]: %d\n", vCopy[1].value);
+   printf("size of a copy: %lu = { %d, %d }\n", (unsigned long)vCopy.size(), vCopy[0].value, vCopy[1].value);
+   assert(vCopy == v0);
 
    printf("\nvalue init ctor:\n");
    testvector v1(10, testtype(-10));
+   assert(v1.size() == 10 && ttl::count(v1.cbegin(), v1.cend(), testtype(-10)) == 10);
+   assert(testvector().size() == 0);
+   assert(testvector(0).size() == 0);
+   assert(testvector(1).size() == 1);
+   assert(testvector(10).size() == 10);
+   assert(testvector(10, testtype(10)).size() == 10);
+   assert(testvector(1, testtype(10))[0] == 10);
 
    printf("\nrw iterator\n");
    for (testvector::iterator i = v1.begin(); i != v1.end(); ++i)
       *i = testtype(i - v1.begin());
+   static const int numbers[] = {0,1,2,3,4,5,6,7,8,9};
+   assert(ttl::equal(v1.cbegin(), v1.cend(), numbers));
 
    printf("\ncopy ctor:\n");
    testvector v2(v1);
    v2.resize(12);
+   assert(v2.size() == 12);
    v2.erase(v2.begin());
+   assert(v2.size() == 11);
    v2.erase(v2.end() - 1);
+   assert(v2.size() == 10);
    for (testvector::const_iterator i = v2.cbegin(); i != v2.cend(); ++i)
       printf(" %d", i->value);
    printf("\n");
+   assert(ttl::equal(v2.cbegin(), v2.cend() - 1, numbers + 1));
 
    v0.swap(v2);
    for (testvector::const_iterator i = v2.cbegin(); i != v2.cend(); ++i)
@@ -87,5 +105,7 @@ void test()
       v.pop_back();
       assert(v.empty());
    }
+
    printf("\ndestructors:\n");
+   testtype::verbose = true;
 }
